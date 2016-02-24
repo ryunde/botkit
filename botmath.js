89c576f03f261e87e8d2adba59543e2b39a50614
tundet <tundet@metropolia.fi>
2016-02-24 11:54:05 +0200

    prime and fibonacci function

--- botmath.js
+++ botmath.js
@@ -1,4 +1,6 @@
-<<<<<<< HEAD
+
+
+
 var isPrime = function (n) {
  if (isNaN(n) || !isFinite(n) || n%1 || n<2) return false; 
  if (n%2==0) return (n==2);
@@ -11,10 +13,33 @@ var isPrime = function (n) {
 
 module.exports.isPrime = isPrime;
 
-=======
+
 var sum = function (num1, num2) {
 	return parseFloat(num1) + parseFloat(num2);
 }
 
 module.exports.sum = sum;
->>>>>>> origin/test
+
+var isFibonacci = function (number) {
+    var prev = 0;
+    var curr = 1;
+    while (prev <= number) {
+        if (prev == number) {
+            return 1;
+        }
+        curr = prev + curr;
+        prev = curr - prev;
+    }
+
+}
+
+module.exports.isFibonacci = isFibonacci;
+
+var findnextFibonacci = function (lowerLimit) {
+
+    for (lowerLimit++; !isFibonacci(lowerLimit); lowerLimit++)
+        ;
+    return lowerLimit;
+}
+
+module.exports.findnextFibonacci = findnextFibonacci;
